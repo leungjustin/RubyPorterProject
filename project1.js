aa409c1117df41b416ec40f2a153a18e54c14774
Justin Leung <jus.leu@network-host.local>
2022-04-04 12:52:24 -0700

    Tried to get active link class to work

--- project1.js
+++ project1.js
@@ -1,31 +1,31 @@
+// TODO: figure out where to declare MenuItem class, so it can be used inside of the class Navbar
+class MenuItem 
+{
+	constructor(name, link, isActive){
+		this.name = name;
+		this.link = link;
+		this.isActive = isActive;
+		this.nestedItems = [];
+	}
+}
+
 class Navbar
 {	
 	constructor(){
-		// TODO: figure out where to declare MenuItem class, so it can be used inside of the class Navbar
-		class MenuItem 
-		{
-			constructor(name, link, isActive){
-				this.name = name;
-				this.link = link;
-				this.isActive = isActive;
-				this.nestedItems = [];
-			}
-		}
+		
 		// fill menuItems with an array of objects that have a name, link, and isActive attribute
 		this.menuItems = [			
-			new MenuItem('Item 1', '#', true),
-			new MenuItem('Item 2', '#', false),
-			new MenuItem('Item 3', '#', false),
-			new MenuItem('Item 4', '#', false),
-			new MenuItem('Item 5', '#', false)
+			new MenuItem('Item 1', '#item1', true),
+			new MenuItem('Item 2', '#item2', false),
+			new MenuItem('Item 3', '#item3', false),
+			new MenuItem('Item 4', '#item4', false),
+			new MenuItem('Item 5', '#item5', false)
 		];
-		this.menuItems[0].nestedItems.push(new MenuItem('Nested Item 1','#', false));
-		this.menuItems[2].nestedItems.push(new MenuItem('Nested Item 1','/', false));
-		this.menuItems[0].nestedItems[0].nestedItems.push(new MenuItem('Another Level', '#', false));		
-		this.logo = "logoideas.jpg";
-		// attempting to figure out how to get nested menu items to be active
-		this.activeTree = [0];
-		
+		this.menuItems[0].nestedItems.push(new MenuItem('Nested Item 1','#nested00', false));
+		this.menuItems[2].nestedItems.push(new MenuItem('Nested Item 1','#nested20', false));
+		this.menuItems[0].nestedItems[0].nestedItems.push(new MenuItem('Another Level', '#nested000', false));		
+		this.logo = "logoideas.jpg";		
+			
 		console.log(this.menuItems);
 		this.bindMethods();
 		this.renderNavbar();
@@ -37,7 +37,7 @@ class Navbar
 	}
 	
 	renderNavbar() {
-		console.log("Render Navbar")		
+		// console.log("Render Navbar")		
 		const itemsHTML = this.menuItems.map((menuItem, index) => this.renderNavItem(menuItem, index)).join(' ');		
 		document.querySelector('.navbar').innerHTML = 
 		`
@@ -47,41 +47,66 @@ class Navbar
 	}
 	
 	renderNavItem(menuItem, index) {
-		console.log("Render Nav Item " + index);						
+		// console.log("Render Nav Item " + index);						
 		let nestedItemHTML = "";
-		console.log("menuItem.nestedItems has items: " + (menuItem.nestedItems.length != 0));		
-		if (menuItem.nestedItems.length != 0 && menuItem.isActive == true)
+		// console.log("menuItem.nestedItems has items: " + (menuItem.nestedItems.length != 0));		
+		if (menuItem.nestedItems.length != 0)
 		{
-			nestedItemHTML = `<ul>${menuItem.nestedItems.map((nestedItem, nestedItemIndex) => this.renderNestedItem(nestedItem, nestedItemIndex, index)).join(' ')}</ul>`;
+			nestedItemHTML = `<ul class="subnav-content">${menuItem.nestedItems.map((nestedItem, nestedItemIndex) => this.renderNestedItem(nestedItem, nestedItemIndex, index)).join(' ')}</ul>`;
 		}		
-		return `<li><a href="${menuItem.link}" class="${menuItem.isActive ? 'active':''}" onclick="navbar.makeNavItemActive(event,${index})">${menuItem.name}</a>${nestedItemHTML}</li>`;		
+		return `<li class="navItem"><a href="${menuItem.link}" class="${menuItem.isActive ? 'active':''}" onclick="navbar.makeNavItemActive(event)">${menuItem.name}</a>${nestedItemHTML}</li>`;		
 	}	
 
 	renderNestedItem(nestedItem, nestedItemIndex, previousItemIndex)	{
-		console.log("Render Nested Item " + nestedItemIndex + " of Nav Item " + previousItemIndex);
+		// console.log("Render Nested Item " + nestedItemIndex + " of Nav Item " + previousItemIndex);
 		let nestedItemHTML = "";		
 		if (nestedItem.nestedItems.length != 0)
 		{
 			// this is recursive
-			nestedItemHTML = `<ul>${nestedItem.nestedItems.map((nestedItem, nestedItemIndex) => this.renderNestedItem(nestedItem, nestedItemIndex, previousItemIndex)).join(' ')}</ul>`;
+			nestedItemHTML = `<ul class="subnav-content">${nestedItem.nestedItems.map((nestedItem, nestedItemIndex) => this.renderNestedItem(nestedItem, nestedItemIndex, previousItemIndex)).join(' ')}</ul>`;
 		}
-		return `<li><a href="${nestedItem.link}" class="${nestedItem.isActive ? 'active':''}">${nestedItem.name}</a>${nestedItemHTML}</li>`
+		return `<li><a href="${nestedItem.link}" class="${nestedItem.isActive ? 'active':''}" onclick="navbar.makeNavItemActive(event)">${nestedItem.name}</a>${nestedItemHTML}</li>`
 
 	}
 	
-	makeNavItemActive(event, index) {		
+	makeNavItemActive(event) {		
 		console.log("change active")		
-		this.makeAllInactive(this.menuItems);
-		console.log(this.menuItems);
-		// TODO: change this so that it changes isActive to true for all the menu items in this.activeTree. Or rethink this strategy
-		this.menuItems[index].isActive = true;
-		this.renderNavbar();
+		this.makeAllInactive(this.menuItems);			
+		// TODO: add active class to menu item whose link matches the current window href
+		this.makeActive(this.menuItems);
+		this.renderNavbar;
 	}	
 
-	makeAllInactive(itemArray) {
+	makeActive(itemArray) {
+		let activeItem;
 		for (let i=0; i < itemArray.length; i++)
 		{
-			itemArray[i].isActive = false;
+			if (window.location.href.includes(itemArray[i].link))
+			{
+				activeItem = document.querySelector(`[href='${itemArray[i].link}']`);
+			}
+			if (itemArray[i].nestedItems.length != 0)
+			{
+				this.makeActive(itemArray[i].nestedItems);
+			}
+		}
+		if(activeItem != null)
+		{
+			activeItem.classList.add('active');
+			console.log(activeItem)
+		}
+		
+	}
+
+	makeAllInactive(itemArray) {
+		let inactiveItem;
+		for (let i=0; i < itemArray.length; i++)
+		{
+			inactiveItem = document.querySelector(`[href='${itemArray[i].link}']`);
+			if (inactiveItem != null)
+			{
+				inactiveItem.classList.remove('active');
+			}
 			if (itemArray[i].nestedItems.length != 0)
 			{
 				// this is recursive
