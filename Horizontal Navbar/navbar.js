a2a7e074c7f31bc564288dedf99dab39688c9bb3
Caleb Donahue <cal.don@network-host.local>
2022-04-11 14:02:32 -0700

    partially changed methods, addNavItem currently problematic

--- Horizontal Navbar/navbar.js
+++ Horizontal Navbar/navbar.js
@@ -1,9 +1,9 @@
 class NavItem {
-	constructor(name, link) {
+	constructor(name, link, isDisabled = false) {
 		this.name = name,
 		this.link = link,
-		this.isDisabled = false, //Determines whether the item can be clicked on.
-		this.isActive = false,
+		this.isActive = false;
+		this.isDisabled = isDisabled, //Determines whether the item can be clicked on.
 		this.subnavItems = []; //Contains an item's subitems, which are also objects of the NavItem class.
 	}
 }
@@ -14,6 +14,7 @@ class NavBar {
 			new NavItem("Item1", "#item1"),
 			new NavItem("Item2", "#item2")
 		]; //A list of objects of the NavItem class.
+
 		this.items[1].subnavItems.push(new NavItem("Subitem1", "#subitem1"));
 		this.items[1].subnavItems.push(new NavItem("Subitem2", "#subitem2"));
 		this.$addForm = document.getElementById("addForm");
@@ -31,29 +32,44 @@ class NavBar {
 		this.$addSubButton = document.getElementById("addSubButton");
 
 		this.$addForm.onsubmit = this.addNavItem.bind(this);
-		this.reload();
+		this.load();
 	}
 
-	//Essentially redraws the navbar and resets forms.
-	reload(hash = window.location.hash) {
-		this.changeActive(hash);
+	//Renders the navbar, sets the active item if there is one, and disables all edit forms.
+	load() {
 		this.fillItems();
+		this.changeActive(window.location.hash);
 		this.addEventListeners();
 		this.disableAll();
 		this.resetForms();
 	}
 
+	//Same as load, expect without rendering the navbar or adding event listeners.
+	reload(hash = window.location.hash) {
+		this.changeActive(hash);
+		this.disableAll();
+		this.resetForms();
+	}
+
+	//Changes which navbar link is active, changing its appearance.
 	changeActive(hash) {
+		//Removes active from all nav links, then makes the link with the matching hash active.
 		this.items.forEach(item => {
 			item.isActive = false;
+			document.getElementById("item"+item.name).classList.remove("active");
 			if (item.link == hash) {
 				item.isActive = true;
+				document.getElementById("item"+item.name).classList.add("active");
 			}
+			//Removes active from all subnav links as well, then makes the link with the matching hash and its parent item active.
 			item.subnavItems.forEach(subitem => {
 				subitem.isActive = false;
+				document.getElementById("subitem"+subitem.name+","+item.name).classList.remove("active");
 				if (subitem.link == hash) {
 					item.isActive = true;
 					subitem.isActive = true;
+					document.getElementById("item"+item.name).classList.add("active");
+					document.getElementById("subitem"+subitem.name+","+item.name).classList.add("active");
 				}
 			});
 		});
@@ -63,28 +79,21 @@ class NavBar {
 	fillItems() {
 		let itemsHTML = `<div class="logo">Logo</div>`;
 		//Each call to renderNavItem adds a new item to the navbar string.
-		itemsHTML += this.items.reduce(
-			(html, item, index) => html += this.renderNavItem(item, index), '');
-			
+		itemsHTML += this.items.reduce((html, item) => html += this.renderNavItem(item), '');
 		document.getElementById("navbar").innerHTML = itemsHTML;
 	}
 
-	//Creates a navbar item and any of its subitems, if it has any, to be placed in the navbar.
-	renderNavItem(item, index) {
+	//Creates a navbar item to be placed in the navbar.
+	renderNavItem(item) {
 		let navString = `
-			<div class="subnav">
-				<a href="${item.link}" class="${item.isActive ? 'active' : ''} ${item.isDisabled ? 'isDisabled' : ''}" id="item${index}">${item.name}</a>
-				<button id="edit${index}">E</button>
-				<div class="subnav-content">
+			<div class="subnav" id="subnav${item.name}">
+				<a href="${item.link}" class="${item.isActive ? 'active' : ''} ${item.isDisabled ? 'isDisabled' : ''}" id="item${item.name}">${item.name}</a>
+				<button id="edit${item.name}">E</button>
+				<div class="subnav-content" id="subnavContent${item.name}">
 		`;
-		//Creates a string for each subitem.
+		//Called for each subitem the item has.
 		for (let i = 0; i < item.subnavItems.length; i++) {
-			navString += `
-				<div>
-					<a href="${item.subnavItems[i].link}" ${item.subnavItems[i].isActive ? 'class="active"' : ''} id="subitem${i},${index}">${item.subnavItems[i].name}</a>
-					<button id="subedit${i},${index}">E</button>
-				</div>
-			`;
+			navString += this.renderSubnavItem(item, i);
 		}
 		navString += `
 				</div>
@@ -93,17 +102,27 @@ class NavBar {
 		return navString;
 	}
 
-	//Adds click and submit events for navbar items and buttons.
+	//Creates a subnav item to be placed within an item.
+	renderSubnavItem(item, i) {
+		return `
+			<div>
+				<a href="${item.subnavItems[i].link}" ${item.subnavItems[i].isActive ? 'class="active"' : ''} id="subitem${item.subnavItems[i].name},${item.name}">${item.subnavItems[i].name}</a>
+				<button id="subedit${item.subnavItems[i].name},${item.name}">E</button>
+			</div>
+		`;
+	}
+
+	//Adds click and submit events for navbar items and buttons. Called on page load.
 	addEventListeners() {
 		//Adds events to each navbar item.
 		for (let i = 0; i < this.items.length; i++) {
 			//Adds events to each item's subitems, if it has any.
 			for (let j = 0; j < this.items[i].subnavItems.length; j++) {
-				document.getElementById("subitem"+j+","+i).onclick = this.reload.bind(this, this.items[i].subnavItems[j].link);
-				document.getElementById("subedit"+j+","+i).onclick = this.editSubnavItem.bind(this, j, i);
+				document.getElementById("subitem"+this.items[i].subnavItems[j].name+","+this.items[i].name).onclick = this.reload.bind(this, this.items[i].subnavItems[j].link);
+				document.getElementById("subedit"+this.items[i].subnavItems[j].name+","+this.items[i].name).onclick = this.editSubnavItem.bind(this, j, i);
 			}
-			document.getElementById("item"+i).onclick = this.reload.bind(this, this.items[i].link);
-			document.getElementById("edit"+i).onclick = this.editNavItem.bind(this, i);
+			document.getElementById("item"+this.items[i].name).onclick = this.reload.bind(this, this.items[i].link);
+			document.getElementById("edit"+this.items[i].name).onclick = this.editNavItem.bind(this, i);
 		}
 	}
 
@@ -135,8 +154,16 @@ class NavBar {
 	//Creates a new navbar item based on user input and then adds it to the item list.
 	addNavItem(event) {
 		event.preventDefault();
+
 		let item = new NavItem(this.$name.value, this.$link.value);
 		this.items.push(item);
+
+		//Renders the new item and adds it to the navbar.
+		document.getElementById("navbar").innerHTML += this.renderNavItem(item);
+		//Add event handlers for new link and edit button.
+		document.getElementById("item"+item.name).onclick = this.reload.bind(this, item.link);
+		document.getElementById("edit"+item.name).onclick = this.editNavItem.bind(this, this.items.length-1);
+		
 		this.reload();
 	}
 
@@ -147,7 +174,6 @@ class NavBar {
 		//Sets the text fields to what the item's name and link are currently.
 		this.$editName.value = this.items[index].name;
 		this.$editLink.value = this.items[index].link;
-
 		//Changes the text of the enable/disable button depending on whether the current item is disabled.
 		if (this.items[index].isDisabled) {
 			this.$enableDisableButton.innerHTML = "Enable Link";
@@ -166,9 +192,22 @@ class NavBar {
 	//Changes the name and/or link of an existing navbar item.
 	submitEdit(index, event) {
 		event.preventDefault();
+
+		let editHTML = document.getElementById("item"+this.items[index].name);
+		
+		//Changes the current item's name and/or link.
 		this.items[index].name = this.$editName.value;
 		this.items[index].link = this.$editLink.value;
-		if (this.items[index].isActive) {
+
+		//Set the properties of the edited item's corresponding html element to match.
+		editHTML.href = this.items[index].link;
+		editHTML.id = "item"+this.items[index].name;
+		editHTML.innerHTML = this.items[index].name;
+		//Binds new event listener for the edited item.
+		editHTML.onclick = this.reload.bind(this, this.items[index].link);
+
+		//If the edited item is the currently active item, updates the hash so it match the update.
+		if (editHTML.classList.contains("active")) {
 			window.location.hash = this.items[index].link;
 		}
 		this.reload();
@@ -176,10 +215,16 @@ class NavBar {
 
 	//Removes an existing navbar item from the list.
 	deleteNavItem(index) {
-		if (this.items[index].isActive) {
+		//If the item being deleted is currently active, removes the hash from the URL.
+		if (document.getElementById("item"+this.items[index].name).classList.contains("active")) {
 			window.location.hash = "";
 		}
+
+		//Removes the item's corresponding html from the navbar.
+		document.getElementById("subnav"+this.items[index].name).remove();
+		
 		this.items.splice(index, 1);
+
 		this.reload();
 	}
 
@@ -187,9 +232,11 @@ class NavBar {
 	enableOrDisableLink(index) {
 		if (this.items[index].isDisabled) {
 			this.items[index].isDisabled = false;
+			document.getElementById("item"+this.items[index].name).classList.remove("isDisabled");
 		}
 		else {
 			this.items[index].isDisabled = true;
+			document.getElementById("item"+this.items[index].name).classList.add("isDisabled");
 		}
 		this.reload();
 	}
@@ -197,8 +244,13 @@ class NavBar {
 	//Creates a new navbar subitem based on user input and then adds it to an existing item's subitem list.
 	addSubnavItem(index, event) {
 		event.preventDefault();
+
 		let subItem = new NavItem(this.$addSubName.value, this.$addSubLink.value)
 		this.items[index].subnavItems.push(subItem);
+
+		document.getElementById("subnavContent"+this.items[index].name).innerHTML += this.renderSubnavItem(this.items[index], this.items[index].subnavItems.length-1);
+		document.getElementById("subitem"+subItem.name+","+this.items[index].name).onclick = this.reload.bind(this, subItem.link);
+		document.getElementById("subedit"+subItem.name+","+this.items[index].name).onclick = this.editSubnavItem.bind(this, this.items[index].subnavItems.length-1, index);
 		this.reload();
 	}
 
