330153ef15a3a2965486e6538f280c349371365d
Justin Leung <leungj3@my.lanecc.edu>
2022-05-25 19:11:01 +0000

    Changed endpoints in index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Content-Type: application/json; charset=utf-8');
 
-$uriArray = explode('?',$path);
+$uriArray = explode('/',$path);
 
 /*
 echo "<pre>";
@@ -25,49 +25,61 @@ print_r($userdata[$indexOfUser]);
 editSettings($userdata);
 try {
 */
-switch($uriArray[0]){
-	case '/users':
-		echo json_encode(dbGetUsers($conn));
-		break;
-	case '/userdata':
-		// Retrieve user record from the db and return to page
-		$user = $_SERVER['QUERY_STRING'];
-		echo json_encode(dbGetUserRecord($user, $conn));
+if ($uriArray[1] == 'users'){
+	// Return list of users
+	echo json_encode(dbGetUsers($conn));
+}
+else if ($uriArray[1] == 'user'){
+	// Add user if no username comes after /user
+	if (is_null($uriArray[2])){
+	        $data = getPageData();
+                $isAdded = dbAddUser($data, $conn);
+                if ($isAdded) {
+                        echo json_encode("user added");
+                }
+                else {
+                        echo json_encode("error");
+                }
+	}
+	else{
+		//If a username is included, check what comes after
+		switch($uriArray[3]){
+			case NULL:
+				// Retrieve user record from the db and return to page
+				$user = $uriArray[2];
+				echo json_encode(dbGetUserRecord($user, $conn));
 
-		break;
-	case '/edit':
-		$data = getPageData();
-		$isUpdated = dbUpdateSettings($data, $conn);
-		if ($isUpdated) {
-			echo json_encode("settings updated");
+				break;
+			case 'edit':
+				// Save user settings
+				$data = getPageData();
+				$isUpdated = dbUpdateSettings($data, $conn);
+				if ($isUpdated) {
+					echo json_encode("settings updated");
+				}
+				else {
+					echo json_encode("error");
+				}
+				break;
+			case 'delete':
+				// Delete user and settings
+				$data = getPageData();
+				$isDeleted = dbDeleteUser($data, $conn);
+				if ($isDeleted) {
+					echo json_encode("user deleted");
+				}
+				else {
+					echo json_encode("error");
+				}
+				break;
+			default:
+				echo 'no data';
+				break;
 		}
-		else {
-			echo json_encode("error");
-		}
-		break;
-	case '/adduser':
-		$data = getPageData();
-		$isAdded = dbAddUser($data, $conn);
-		if ($isAdded) {
-			echo json_encode("user added");
-		}
-		else {
-			echo json_encode("error");
-		}
-		break;
-	case '/deleteuser':
-		$data = getPageData();
-		$isDeleted = dbDeleteUser($data, $conn);
-		if ($isDeleted) {
-			echo json_encode("user deleted");
-		}
-		else {
-			echo json_encode("error");
-		}
-		break;
-	default:
-		echo 'no data';
-		break;
+	}
+}
+else {
+	echo 'no data';
 }
 /*
 } catch(\Exception $e) {
